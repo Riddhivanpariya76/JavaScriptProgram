fb6c62580e9088788d0b990b200a2edce0d60ec0
Riddhivanpariya76 <riddhivanpariya76@gmail.com>
2026-01-29 13:34:37 +0530

    [RV][added]

--- Object.js
+++ Object.js
@@ -21,8 +21,27 @@
 // console.log(person)
 
 // nested object
-const person1 = {
-    name : 'john',
+// const person1 = {
+//     name : 'john',
+//     age : 20,
+//     address :{
+//         city : "keshod",
+//         zipcode : '362220',
+//         street : "123 main st."
+//     }
+// }
+// console.log(person1)
+// console.log(person1?.address?.city)
+// console.log(Object.keys(person1))
+// console.log(Object.values(person1))
+// console.log(Object.entries(person1))
+
+// destructure object
+
+const user = {
+    name : "user",
+    role : "Admin",
+    designation :"developer",
     age : 20,
     address :{
         city : "keshod",
@@ -30,8 +49,53 @@ const person1 = {
         street : "123 main st."
     }
 }
-console.log(person1)
-console.log(person1?.address?.city)
-console.log(Object.keys(person1))
-console.log(Object.values(person1))
-console.log(Object.entries(person1))
+// const {name,age,role} = user
+// console.log(name,age,role)
+
+// rename object properties
+// const {name : profilNmae, age : profileAge} = user
+// console.log(profilNmae, profileAge)
+
+// set default value
+// const {name, age, role, state = "Gujarat"} = user
+// console.log(name, age, role, state)
+
+// const array = [1,2,3]
+// const moreElements = [...array,4,5,6]
+// console.log(moreElements)
+
+//copied array
+// const originalArrray = [1,2,3]
+// const copiedArray = [...originalArrray]
+// console.log(copiedArray)
+
+// merge array
+// const arr1 = [1,2,3]
+// const arr2 = [4,5,6]
+// const mergeArray = [...arr1,...arr2]
+// console.log(mergeArray)
+
+// spread operator
+// const user1 = {
+//     name : "user1",
+//     city : "keshod",
+// }
+// console.log(user1)
+// const newObj = {
+//     ...user1,
+//     role : "React Developer"
+// }
+// console.log(newObj)
+
+//merge obj
+const obj1 = {
+    name : "name",
+    age :20
+}
+const obj2 = {
+    city : "keshod",
+    state : "gujarat",
+    age : 30
+}
+const obj3 = {...obj1,...obj2}
+console.log(obj3)
---
+++ task.js
@@ -0,0 +1,10 @@
+let Student = {
+    name : " first name ",
+    email : "USER@GMAIL.COM",
+    course : "JavaScript, React, Node",
+    marks : [45, 78, 60, 90, 33],
+    hobbies : ["Music", "Cricket", "Reading"],
+    address : ["India", ["Gujarat", "Ahmedabad"]]
+};
+
+// STRING OPERATIONS
