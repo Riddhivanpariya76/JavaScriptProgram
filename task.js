94b74c0ed9906452139bf8a4996b1e0ddaf4fa80
Riddhivanpariya76 <riddhivanpariya76@gmail.com>
2026-01-30 00:13:51 +0530

    [RV][two files are added]

--- task.js
+++ task.js
@@ -7,7 +7,7 @@ let Student = {
     address : ["India", ["Gujarat", "Ahmedabad"]]
 };
 
-// STRING OPERATIONS
+// STRING OPERATIONS //
 // remove space
 console.log("trim() : ",Student.name)
 
@@ -27,9 +27,78 @@ console.log("indexOf : ",Student.course.indexOf("Node"));
 console.log("replace() : ",Student.course.replace("JavaScript","JS"))
 
 // convert course string into an array
+console.log("convert into array : ",Student.course.split(", "))
 
 //display the length of student name
 console.log("length() : ",Student.name.length)
 
-// ARRAY OPERATION
+// ARRAY OPERATION //
 //add "Swimming" to hobbies
+Student.hobbies.push("Swimming");
+console.log(Student.hobbies);
+
+// remove the last hobby
+Student.hobbies.pop()
+console.log('remove last hobbie : ',Student.hobbies)
+
+// add "Drawing" at the beginning of hobbies
+Student.hobbies.unshift("Drawing")
+console.log('beginning of hobbie : ',Student.hobbies)
+
+// remove the first hobby
+Student.hobbies.shift()
+console.log('remove first hobbie : ',Student.hobbies)
+
+// merge hobbies with course
+const Student1 = {
+    hobbies : ["Music", "Cricket", "Reading"],
+    course : "JavaScript, React, Node"
+};
+const mergeArray = [...Student.hobbies, ...Student.course.split(", ")];
+console.log(mergeArray)
+
+// check if "React" exit
+// console.log("check React : ",Student.course.includes("React"))
+
+// find index of "JS" in course array
+console.log("index of JS : ",Student.course.includes("JS"))
+
+// extract first two marks
+
+// remove one failed subject mark (less than 40) using splice
+
+// convert hobbies array into a string
+console.log("convert array into srting : ",Student.hobbies.join(", "))
+
+// ARRAY ITERATION METHOD //
+console.log("array iteration method :")
+let marks = [45, 78, 60, 90, 33];
+
+// use map() to add 5 bounds marks to each mark
+console.log("add 5 bounds marks : ",marks.map(num => num + 5))
+console.log("old array : ",marks)
+
+// use filter() to get only passed marks (>=40)
+console.log("passed marks (>=40) : ",marks.filter(num => num > 40))
+console.log("old array : ",marks)
+
+// use finnd() to find the first mark above 80
+console.log("first mark above 80 : ", marks.find(num => num > 80));
+
+// use forEach() to print each hobby
+console.log("print hobbies : ")
+Student.hobbies.forEach(hobby => console.log(hobby))
+
+// sort mark in ascending order
+console.log("sort mark in asceding order : ",marks.sort());
+
+// reverse mark order
+console.log("reverse mark ordeer : ",marks.reverse())
+
+// flatten the address array
+
+
+// OBJECT HANDLING //
+// add a new property result = "Pass" if all marks are >=40
+
+// print final student object
